ba64ba81ea0c25595e7030e275ec6c1f5211ab74
xeldrago <jeffrynspr@gmail.com>
2022-02-05 23:29:43 +0530

    done with formatting the code

--- CMtoMT.cpp
+++ CMtoMT.cpp
@@ -1,18 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-
 
 int main()
 {
-float cm;
-float mt;
-cout<<"Enter the value of centimeters\n";
-cin >>cm;
+    float cm;
+    float mt;
+    cout << "Enter the value of centimeters\n";
+    cin >> cm;
 
-mt=cm/100;
-cout<<"The value of given centimeter in meter is :"<<mt;
-
+    mt = cm / 100;
+    cout << "The value of given centimeter in meter is :" << mt;
 }
-
-
