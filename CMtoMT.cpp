764975eb8d97da14621b9f57e8f2791c1be73d56
xeldrago <jeffrynspr@gmail.com>
2022-02-05 18:34:41 +0530

    fixed umm.. the centimeter to meter thing

--- CMtoMT.cpp
+++ CMtoMT.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-int cm;
+float cm;
 float mt;
 cout<<"Enter the value of centimeters\n";
-int cin >>cm;
+cin >>cm;
 
 mt=cm/100;
 cout<<"The value of given centimeter in meter is :"<<mt;
-return 0;
+
 }
 
 
